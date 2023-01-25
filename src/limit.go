283233479d01047c5db7eb36da910ccce5947cf2
Yosebu <thedjons@gmail.com>
2023-01-25 17:21:03 +0700

    feat: added subnet based rate limiting

---
+++ src/limit.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+// Create a custom visitor struct which holds the rate limiter for each
+// visitor and the last time that the visitor was seen.
+type visitor struct {
+	limiter  *rate.Limiter
+	lastSeen time.Time
+}
+
+// Change the the map to hold values of the type visitor.
+var visitors = make(map[string]*visitor)
+var mu sync.Mutex
+
+// Run a background goroutine to remove old entries from the visitors map.
+func init() {
+	go cleanupVisitors()
+}
+
+func getVisitor(ip string) *rate.Limiter {
+	mu.Lock()
+	defer mu.Unlock()
+
+	v, exists := visitors[ip]
+	if !exists {
+		limiter := rate.NewLimiter(rate.Every(2*time.Minute), 100)
+		// Include the current time when creating a new visitor.
+		visitors[ip] = &visitor{limiter, time.Now()}
+		return limiter
+	}
+
+	// Update the last seen time for the visitor.
+	v.lastSeen = time.Now()
+	return v.limiter
+}
+
+// Every minute check the map for visitors that haven't been seen for
+// more than 3 minutes and delete the entries.
+func cleanupVisitors() {
+	for {
+		time.Sleep(time.Minute)
+
+		mu.Lock()
+		for ip, v := range visitors {
+			if time.Since(v.lastSeen) > 2*time.Minute {
+				delete(visitors, ip)
+			}
+		}
+		mu.Unlock()
+	}
+}
+
+func limit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip := r.Header.Get("X-Forwarded-For")
+		mask := "/24"
+		_, ipv4Net, err := net.ParseCIDR(ip + mask)
+		if err != nil {
+			http.Error(w, http.StatusText(502), http.StatusInternalServerError)
+			return
+		}
+
+		limiter := getVisitor(ipv4Net.String())
+		fmt.Println(ipv4Net.String())
+		if limiter.Allow() == false {
+			http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
--- src/main.go
+++ src/main.go
@@ -1,44 +1,18 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
-	"sync"
 	"time"
 )
 
-type countHandler struct {
-	mu sync.Mutex
-	n  map[string]interface{}
-}
-
-var c = countHandler{n: map[string]interface{}{"counter": 0}}
-
-func (c *countHandler) serveHTML(w http.ResponseWriter, filename string) {
-	c.mu.Lock()
-	c.n["counter"] = c.n["counter"].(int) + 1
-	t, err := template.ParseFiles(filename)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	defer c.mu.Unlock()
-	if err := t.Execute(w, c.n); err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-}
-
-func mainHandler(w http.ResponseWriter, r *http.Request) {
-	c.serveHTML(w, "static/index.html")
-}
-
 func main() {
 
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("./static")))
 	server := &http.Server{
 		Addr:           ":8080",
-		Handler:        http.HandlerFunc(mainHandler),
+		Handler:        limit(mux),
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
